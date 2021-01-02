12a9687260e87f716f5ea7a79ce7d11e6e818384
SaltineChips <rubixblockchain@gmail.com>
2021-01-01 22:30:55 -0800

    Updated checkpoints
    -Added checkpoints so the network doesn't fracture.

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -35,9 +35,20 @@ namespace Checkpoints
     //    timestamp before)
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-               ( 0, uint256("0x2cadd7467aa4ca55a8be084aa8779da4f250981cbd7a7e377a4502bb58e5578b"))
-			   ( 1, uint256("0x4f8e827684f70fb6d731c92a401572e180d0ebc35932fc617e9c717cdee7e9b0"))
-			//   ( 2, uint256("0x2cadd7467aa4ca55a8be084aa8779da4f250981cbd7a7e377a4502bb58e5578b"))
+               ( 0,       uint256("0x2cadd7467aa4ca55a8be084aa8779da4f250981cbd7a7e377a4502bb58e5578b"))
+			   ( 1,       uint256("0x4f8e827684f70fb6d731c92a401572e180d0ebc35932fc617e9c717cdee7e9b0"))
+			   ( 2,       uint256("0xc388466de0b8cc23e70c779afdd35e2f90bac606193a54b6c99b0be789189a8e"))
+               ( 500,     uint256("0x5cf4d300bce5076aa32a2ddf2376c4be335d9656b054c0433124cd2402ed7c48"))
+               ( 5000,    uint256("0x5299233cbc9ef9793a43ef239471a1e7f01ce8262c2d0e1c74bf4a9be1026919"))
+               ( 25000,   uint256("0xf423e43134b7c014f5e5d8b8db999bdd6df7d2b038255f9520bb2c3073c55110"))
+               ( 50000,   uint256("0xef707bfb28061129e4c0aeda5f8e8fd8fb9f10df42ccc51e51ff17b8b7563da7"))
+               ( 100000,  uint256("0x16f93f7e3974422e1a68bd8a6eb3f2d5776e7db72b8cc8a08946081cac253dce"))
+               ( 125000,  uint256("0x8bd440c1238c76264734ea05436454fd3873fb5acce1684c042031a6ee18cddf"))
+               ( 500000,  uint256("0xf57dde1116078c96c2f4250205cbb61d17b2bf9045a7fa008881d153937f0492"))
+               ( 750000,  uint256("0x6c4bd350bda75f32ab746c7ad78752881c821ce7cdb7835ca5535d2c455b1144"))
+               ( 1000000, uint256("0xa71fdfa33f4278974dc99fd2b41a73b285fb8807c6ae474184429adc4599a8a5"))
+               ( 1250000, uint256("0x13a323e1005cb3486f3b1b277181371cbb450cf820d2e77e0fa9600716cad324"))
+               ( 1500000, uint256("0x24f1fc28a50fc57d37a68cc5e3a0d957047c9f7170d74ea036b7627414ce4df1")) 
 	;
     static const CCheckpointData data = {
         &mapCheckpoints,
